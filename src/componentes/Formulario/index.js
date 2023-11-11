62f5313db2ccbaaa14f7a81ec30737af983934ab
André Guimarães <andrepaivaguimaraes@gmail.com>
2023-11-11 17:36:59 -0300

    Form completo

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,25 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
 import './Formulario.css'
 
 const Formulario = () => {
+    const times = ['Programação', 'Front-End', 'Data Science', 'DevOps','UX e Design','Inovação e Gestão']
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('form foi submetido')
+    }
+
     return (
-        <section>
-            <form className='formulario'>
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}></ListaSuspensa>
+                <Botao>Criar Card</Botao>
             </form>
         </section>
     )
